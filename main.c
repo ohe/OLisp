a27d1d644ba96f05b90a228076460a023570909e
Olivier Hervieu <olivier.hervieu@gmail.com>
2017-04-04 08:06:21 +0200

    Error Handling

--- main.c
+++ main.c
@@ -6,29 +6,86 @@
 
 #include "mpc.h"
 
+enum LVAL_TYPE {LVAL_NUM, LVAL_ERR};
+enum ERROR_TYPE {LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM};
 
-long eval_op(long x, char *op, long y) {
-    if (strncmp(op, "+", 1) == 0) return x + y;
-    if (strncmp(op, "-", 1) == 0) return x - y;
-    if (strncmp(op, "*", 1) == 0) return x * y;
-    if (strncmp(op, "/", 1) == 0) return x / y;
-    if (strncmp(op, "%", 1) == 0) return x % y;
-    if (strncmp(op, "^", 1) == 0) return pow(x, y);
-    if (strncmp(op, "min", 3) == 0) return x < y? x: y;
-    if (strncmp(op, "max", 3) == 0) return x > y? x: y;
+typedef struct {
+    int type;
+    /* Union are more efficient in memory as they create space for its largest element
+     * the counterpart is that only one field can be created/accessed at the same time
+     * This fits the lval struct because, depending of the context, only num or err will
+     * be set
+     */
+    union {
+        long num;
+        int err;
+    };
+} lval;
+
+lval lval_num(long x) {
+    lval v;
+    v.type = LVAL_NUM;
+    v.num = x;
+    return v;
 }
 
-long eval(mpc_ast_t *t) {
+lval lval_err(int x) {
+    lval v;
+    v.type = LVAL_ERR;
+    v.err = x;
+    return v;
+}
+
+void lval_print(lval v) {
+    switch (v.type) {
+        case LVAL_NUM:
+            printf("%li", v.num);
+            break;
+        case LVAL_ERR:
+            if (v.err == LERR_DIV_ZERO) printf("Error: Division by zero!");
+            if (v.err == LERR_BAD_OP) printf("Error: Invalid Operator!");
+            if (v.err == LERR_BAD_NUM) printf("Error: Invalid Number!");
+            break;
+    }
+}
+void lval_println(lval v) {
+    lval_print(v);
+    putchar('\n');
+}
+
+lval eval_op(lval x, char *op, lval y) {
+    if (x.type == LVAL_ERR) return x;
+    if (y.type == LVAL_ERR) return y;
+    if (strncmp(op, "+", 1) == 0) return lval_num(x.num + y.num);
+    if (strncmp(op, "-", 1) == 0) return lval_num(x.num - y.num);
+    if (strncmp(op, "*", 1) == 0) return lval_num(x.num * y.num);
+    if (strncmp(op, "/", 1) == 0) return y.num == 0 ? lval_err(LERR_DIV_ZERO): lval_num(x.num / y.num);
+    if (strncmp(op, "%", 1) == 0) return lval_num(x.num % y.num);
+    if (strncmp(op, "^", 1) == 0) return lval_num(pow(x.num, y.num));
+    if (strncmp(op, "add", 1) == 0) return lval_num(x.num + y.num);
+    if (strncmp(op, "sub", 1) == 0) return lval_num(x.num - y.num);
+    if (strncmp(op, "mul", 1) == 0) return lval_num(x.num * y.num);
+    if (strncmp(op, "div", 1) == 0) return y.num == 0 ? lval_err(LERR_DIV_ZERO): lval_num(x.num / y.num);
+    if (strncmp(op, "min", 3) == 0) return x.num < y.num? x: y;
+    if (strncmp(op, "max", 3) == 0) return x.num > y.num? x: y;
+
+    return lval_err(LERR_BAD_OP);
+
+}
+
+lval eval(mpc_ast_t *t) {
     if(strstr(t->tag, "number")) {
-        return atoi(t->contents);
+        errno = 0;
+        long x = strtol(t->contents, NULL, 10);
+        return errno == ERANGE ? lval_err(LERR_BAD_NUM): lval_num(x);
     }
 
     char *op = t->children[1]->contents;
-    long x = eval(t->children[2]);
+    lval x = eval(t->children[2]);
 
     int i = 3;
     if (strstr(t->children[i]->tag, "expr") == NULL && strncmp(op, "-", 1) == 0) {
-        return -x;
+        return lval_num(-x.num);
     } else {
         /* iterate the remaining children and combining */
         while (strstr(t->children[i]->tag, "expr")) {
@@ -49,11 +106,11 @@ int main(int argc, char **argv) {
     mpc_result_t result;
 
     mpca_lang(MPCA_LANG_DEFAULT,
-              "                                                                       \
-               number   : /-?[0-9]+/;                                                 \
-               operator : '+' | '-' | '*' | '/' | '%' | '^' | /min/ | /max/;          \
-               expr     : <number> | '(' <operator> <expr>+ ')';                      \
-               olisp    : /^/ <operator><expr>+ /$/;                                  \
+              "                                                                                              \
+               number   : /-?[0-9]+/;                                                                       \
+               operator : '+' | '-' | '*' | '/' | '%' | '^' | /min/ | /max/ | /add/ | /sub/ | /mul/ | /div/; \
+               expr     : <number> | '(' <operator> <expr>+ ')';                                             \
+               olisp    : /^/ <operator><expr>+ /$/;                                                         \
               ",
               Number, Operator, Expr, OLisp);
 
@@ -65,7 +122,8 @@ int main(int argc, char **argv) {
         add_history(input);
 
         if (mpc_parse("<stdin>", input, OLisp, &result)) {
-            printf("%li\n", eval(result.output));
+            mpc_ast_print(result.output);
+            lval_println(eval(result.output));
             mpc_ast_delete(result.output);
         } else {
             mpc_err_print(result.error);
